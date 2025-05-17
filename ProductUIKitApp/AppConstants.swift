648959182cea20e8a4c99f2af962660ad1d9e50f
Zeeshan Shakeel <zeeshan@Zeeshans-MacBook-Pro.local>
2025-05-17 18:50:05 +0500

    app constants and some refactroing

---
+++ ProductUIKitApp/AppConstants.swift
@@ -0,0 +1,29 @@
+//
+//  AppConstants.swift
+//  ProductUIKitApp
+//
+//  Created by Zeeshan Shakeel on 17/05/2025.
+//
+
+import Foundation
+
+// MARK: - App Constants (Enum for name spacing)
+enum AppConstants {
+    // MARK: - API
+    enum API {
+        static let baseURLString = "https://dummyjson.com"
+        static let productsEndpoint = "/products"
+        static let usersEndpoint = "/users"
+        // ... other API endpoints
+    }
+    
+    // MARK: - UI
+    enum UI {
+        static let defaultPadding: CGFloat = 16.0
+        static let animationDuration: TimeInterval = 0.3
+    }
+    
+    // MARK: - Other
+    static let appVersion = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String ?? "N/A"
+    static let supportEmail = "support@yourapp.com"
+}
--- ProductUIKitApp/Services/ProductService.swift
+++ ProductUIKitApp/Services/ProductService.swift
@@ -16,7 +16,8 @@ protocol IProductService {
 class ProductService: IProductService {
     
     func fetchProducts(skip: Int, limit: Int, completion: @escaping (Result<ProductResponse, AFError>) -> Void) {
-        let urlString = "https://dummyjson.com/products?skip=\(skip)&limit=\(limit)"
+        //let urlString = "https://dummyjson.com/products?skip=\(skip)&limit=\(limit)"
+        let urlString = "\(AppConstants.API.baseURLString)\(AppConstants.API.productsEndpoint)?skip=\(skip)&limit=\(limit)"
         
         AF.request(urlString).responseDecodable(of: ProductResponse.self) { response in
             completion(response.result)
